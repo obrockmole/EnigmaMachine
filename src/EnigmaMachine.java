3f2955e02e784616d608bf6cf3c72b46da685630
obrockmole <86027400+obrockmole@users.noreply.github.com>
2025-01-15 14:04:51 -0500

    Very rough outline of process

--- src/EnigmaMachine.java
+++ src/EnigmaMachine.java
@@ -1,5 +1,59 @@
 public class EnigmaMachine {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public EnigmaMachine() {
+
+    }
+
+    private void setRotors() {
+
+    }
+
+    private void setRotorPositions() {
+
+    }
+
+    private void setRingSettings() {
+
+    }
+
+    private void setReflector() {
+
+    }
+
+    private void setPlugboard() {
+
+    }
+
+    private void setMessage() {
+
+    }
+
+
+
+    private void turnRotors() {
+
+    }
+
+    private char passThroughPlugboard(char letter) {
+        return '0';
+    }
+
+    private char passThroughRotors(char letter) {
+        return '0';
+    }
+
+    private char passThroughReflector(char letter) {
+        return '0';
+    }
+
+
+
+    private String encodeMessage() {
+        passThroughPlugboard('0');
+        passThroughRotors('0');
+        passThroughReflector('0');
+        passThroughRotors('0');
+        passThroughPlugboard('0');
+
+        return null;
     }
 }
