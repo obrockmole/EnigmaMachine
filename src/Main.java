7d59c75dd76b4febbd265bb4b656e45aacb9f2cc
obrockmole <86027400+obrockmole@users.noreply.github.com>
2025-02-07 14:23:55 -0500

    Created main program to test the enigma

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String message = "aaaaa";
+
+        EnigmaMachine enigma = new EnigmaMachine(
+            new int[] {1, 2, 3},
+            new char[] {'A', 'A', 'A'},
+            new char[] {'A', 'A', 'A'},
+            'B',
+            new String[] {},
+            message
+        );
+
+        System.out.printf("Original message: %s\n", message);
+        System.out.printf("New message: %s\n", enigma.encodeMessage());
+    }
+}
